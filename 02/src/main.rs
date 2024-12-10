2322a92166a4c472a18e442a4964bb97c52f9140
Santiago Ontañón Sánchez <sontanon@gmail.com>
2024-12-10 14:04:05 -0600

    Improve solution

--- 02/src/main.rs
+++ 02/src/main.rs
@@ -17,74 +17,6 @@ fn valid_level(level: &[i32]) -> bool {
     })
 }
 
-// fn valid_level_by_removing_one(level: &[i32]) -> bool {
-//     // If the slice has less than 2 elements, it is an automatic success.
-//     if level.len() < 2 {
-//         return true;
-//     }
-//
-//     // Index to store the index of the first violation if any exists.
-//     let mut invalid_pair: Option<usize> = None;
-//
-//     // Determine if the slice is ascending or descending.
-//     let ascending = level[0] < level[1];
-//
-//     // Iterate over the pairs in the level.
-//     // The enumeration allows us to keep track of the index in the slice.
-//     for (k, pair) in level.windows(2).enumerate() {
-//         let i = pair[0];
-//         let j = pair[1];
-//         if !valid_condition(i, j, ascending) {
-//             invalid_pair = Some(k);
-//             break;
-//         }
-//     }
-//
-//     // If the loop completes with no violation, we can return true.
-//     if invalid_pair.is_none() {
-//         return true;
-//     }
-//
-//     // Otherwise, the slice may be valid if we remove one of the elements.
-//     let k = invalid_pair.unwrap();
-//
-//     // Edge cases:
-//     // The violating pair is the last one. This is an automatic success by removing the last element
-//     if k == level.len() - 2 {
-//         return true;
-//     }
-//     // Otherwise, k must be less than the length of the slice - 2 (so we can safely access k + 2).
-//
-//     // The violating pair is the first one.
-//     if k == 0 {
-//         return boundary_condition(level[0], &level[2..], level[0] < level[2])
-//             || valid_level(&level[1..]);
-//     }
-//
-//     // The violating pair is the second one.
-//     // And the ascending direction changed.
-//     if k == 1 && (level[0] < level[1]) != (level[1] < level[2]) {
-//         if valid_level(&level[1..]) {
-//             return true;
-//         }
-//     }
-//
-//     // There are now two choices:
-//     // 1. Remove the right element, i.e., k + 1.
-//     let remove_right = {
-//         let i = level[k];
-//         boundary_condition(i, &level[k + 2..], ascending)
-//     };
-//
-//     // 2. Remove the left element, i.e., k.
-//     let remove_left = {
-//         let i = level[k - 1];
-//         boundary_condition(i, &level[k + 1..], ascending)
-//     };
-//
-//     remove_left || remove_right
-// }
-
 fn valid_condition(i: i32, j: i32, ascending: bool) -> bool {
     if ascending {
         (i < j) && (j - i) <= 3
@@ -92,18 +24,6 @@ fn valid_condition(i: i32, j: i32, ascending: bool) -> bool {
         (i > j) && (i - j) <= 3
     }
 }
-
-// fn boundary_condition(i: i32, slice: &[i32], ascending: bool) -> bool {
-//     match slice.len() {
-//         0 => panic!("Slice must have at least one element"),
-//         1 => valid_condition(i, slice[0], ascending),
-//         2.. => {
-//             valid_condition(i, slice[0], ascending)
-//                 && ((i < slice[0]) == (slice[0] < slice[1]))
-//                 && valid_level(slice)
-//         }
-//     }
-// }
 
 fn exercise_1(path: &Path) -> Result<i32, Box<dyn std::error::Error>> {
     let input = File::open(path)?;
@@ -142,37 +62,27 @@ fn exercise_2(path: &Path) -> Result<i32, Box<dyn std::error::Error>> {
 
     let exploded_levels: Vec<Vec<Vec<i32>>> = levels?
         .iter()
-        .map(
-            |level|  -> Vec<Vec<i32>> {
-                (0..level.len())
-                    .map(
-                        |skip_idx| -> Vec<i32> {
-                            level
-                                .iter()
-                                .enumerate()
-                                .filter(|(i, slice)| *i != skip_idx)
-                                .map(|(_, x)| *x)
-                                .collect()
-                        }
-                    )
-                    .collect()
-            }
-        )
+        .map(|level| -> Vec<Vec<i32>> {
+            (0..level.len())
+                .map(|skip_idx| -> Vec<i32> {
+                    level
+                        .iter()
+                        .enumerate()
+                        .filter(|(i, _)| *i != skip_idx)
+                        .map(|(_, &x)| x)
+                        .collect()
+                })
+                .collect()
+        })
         .collect();
 
     let valid_levels: Vec<bool> = exploded_levels
         .iter()
-        .map(
-            |exploded_level| -> bool {
-                exploded_level
-                    .iter()
-                    .any(
-                        |level| -> bool {
-                            valid_level(level)
-                        }
-                    )
-            }
-        )
+        .map(|exploded_level| -> bool {
+            exploded_level
+                .iter()
+                .any(|level| -> bool { valid_level(level) })
+        })
         .collect();
 
     Ok(valid_levels.iter().filter(|&&valid| valid).count() as i32)
