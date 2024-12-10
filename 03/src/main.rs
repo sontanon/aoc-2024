91e1bbdad7c81068c1b8f590e6e51fd5f5688ff3
Santiago Ontañón Sánchez <sontanon@gmail.com>
2024-12-10 15:05:21 -0600

    Completed day 3

--- 03/src/main.rs
+++ 03/src/main.rs
@@ -2,38 +2,72 @@ use regex::Regex;
 use std::fs::{self};
 use std::path::Path;
 
-fn exercise_1(input_text: &str) -> i32 {
-    let pattern = Regex::new(r"mul\((\d{1,3}),(\d{1,3})\)").unwrap();
+fn exercise_1(input_text: &str) -> Result<i32, Box<dyn std::error::Error>> {
+    let pattern = Regex::new(r"mul\((\d{1,3}),(\d{1,3})\)")?;
 
-    let sum: i32 = pattern.captures_iter(&input_text).map(|cap| {
-        let a = cap[1].parse::<i32>().unwrap();
-        let b = cap[2].parse::<i32>().unwrap();
-        a * b
-    }).sum();
-
-    sum
+    pattern.captures_iter(input_text).map(|cap| {
+        let a = cap.get(1).ok_or("Missing first argument")?.as_str().parse::<i32>()?;
+        let b = cap.get(2).ok_or("Missing second argument")?.as_str().parse::<i32>()?;
+        Ok(a * b)
+    }).sum()
 }
 
-fn exercise_2(input_text: &str) -> i32 {
-    let pattern = Regex::new(r"do\(\)|don't\(\)").unwrap();
+#[derive(Debug)]
+enum Operation {
+    Mul(i32, i32),
+    Do,
+    DoNot
+}
 
-    let splits: Vec<&str> = pattern.split(&input_text).collect();
+fn exercise_2(input_text: &str) -> Result<i32, Box<dyn std::error::Error>> {
+    let pattern = Regex::new(r"(do\(\)|don't\(\)|mul\((\d{1,3}),(\d{1,3})\))")?;
 
-    let operations =
+    let operations: Result<Vec<Operation>, _> = pattern.captures_iter(input_text).map(|cap| -> Result<Operation, Box<dyn std::error::Error>> {
+        match cap.get(1).ok_or("Missing operation")?.as_str() {
+            "do()" => Ok(Operation::Do),
+            "don't()" => Ok(Operation::DoNot),
+            _ => {
+                let a = cap.get(2).ok_or("Missing first argument")?.as_str().parse::<i32>()?;
+                let b = cap.get(3).ok_or("Missing second argument")?.as_str().parse::<i32>()?;
+                Ok(Operation::Mul(a, b))
+            }
+        }
+    }).collect();
 
-    todo!()
 
+    let mut sum = 0;
+    let mut do_operation = true;
+
+    for op in operations? {
+        match op {
+            Operation::Mul(a, b) => {
+                if do_operation {
+                    sum += a * b;
+                }
+            },
+            Operation::Do => {
+                do_operation = true;
+            },
+            Operation::DoNot => {
+                do_operation = false;
+            }
+        }
+    }
+
+    Ok(sum)
 }
 
 
-fn main() {
+fn main() -> Result<(), Box<dyn std::error::Error>> {
     let path = Path::new("input.txt");
     let input_text = fs::read_to_string(path).expect("Failed to read file");
 
-    let sum_1 = exercise_1(&input_text);
-    let sum_2 = exercise_2(&input_text);
+    let sum_1 = exercise_1(&input_text)?;
+    let sum_2 = exercise_2(&input_text)?;
 
     println!("Sum 1: {}", sum_1);
     println!("Sum 2: {}", sum_2);
+
+    Ok(())
 
 }
