e7e360b99e9e917381b74672dbc543e12872bc5b
Santiago Ontañón Sánchez <sontanon@gmail.com>
2024-12-15 22:47:08 -0600

    Completed day 8

--- 08/src/main.rs
+++ 08/src/main.rs
@@ -4,7 +4,7 @@ use std::{
     fs,
 };
 
-#[derive(Debug)]
+#[derive(Debug, PartialEq)]
 struct AntennaCell {
     antenna: Option<char>,
     coords: (usize, usize),
@@ -31,10 +31,10 @@ impl AntennaGrid {
         let cells: Vec<Vec<AntennaCell>> = input_str
             .lines()
             .enumerate()
-            .map(|(i, line)| {
+            .map(|(j, line)| {
                 line.chars()
                     .enumerate()
-                    .map(|(j, c)| match c {
+                    .map(|(i, c)| match c {
                         '.' => AntennaCell {
                             antenna: None,
                             coords: (i, j),
@@ -56,7 +56,7 @@ impl AntennaGrid {
     }
 }
 
-fn exercise_1(antenna_grid: &AntennaGrid) -> Result<usize> {
+fn preprocessing(antenna_grid: &AntennaGrid) -> Result<HashMap<char, Vec<&AntennaCell>>> {
     let unique_antennas: HashSet<char> = antenna_grid
         .cells
         .iter()
@@ -79,6 +79,10 @@ fn exercise_1(antenna_grid: &AntennaGrid) -> Result<usize> {
         })
         .collect();
 
+    Ok(antennas)
+}
+
+fn build_pairs<'a>(antennas: &'a HashMap<char, Vec<&'a AntennaCell>>) -> Result<HashMap<char, Vec<(&'a AntennaCell, &'a AntennaCell)>>> {
     let antenna_pairs: HashMap<char, Vec<(&AntennaCell, &AntennaCell)>> = antennas
         .iter()
         .filter_map(|(antenna, cells)| {
@@ -86,8 +90,8 @@ fn exercise_1(antenna_grid: &AntennaGrid) -> Result<usize> {
                 return None;
             }
             let cell_pairs: Vec<(&AntennaCell, &AntennaCell)> = (0..cells.len() - 1)
-                .flat_map(|i| -> Vec<(&AntennaCell, &AntennaCell)> {
-                    (i + 1..cells.len()).map(|j| (cells[i], cells[j])).collect()
+                .flat_map(|k| -> Vec<(&AntennaCell, &AntennaCell)> {
+                    (k + 1..cells.len()).map(|l| (cells[k], cells[l])).collect()
                 })
                 .collect();
             Some((*antenna, cell_pairs))
@@ -98,6 +102,144 @@ fn exercise_1(antenna_grid: &AntennaGrid) -> Result<usize> {
         antenna_pairs.values().all(|pairs| !pairs.is_empty()),
         "No antenna pairs"
     );
+
+    Ok(antenna_pairs)
+}
+
+enum Direction {
+    //    (2)
+    //    ^
+    //   /
+    // (1)
+    NE,
+    // (1)
+    //   \
+    //    v
+    //    (2)
+    SE,
+    //    (1)
+    //    /
+    //   v
+    // (2)
+    SW,
+    // (2)
+    //   ^
+    //    \
+    //    (1)
+    NW,
+}
+
+impl Direction {
+    fn determine_direction(p_1: (usize, usize), p_2: (usize, usize)) -> Self {
+        let (x_1, y_1) = p_1;
+        let (x_2, y_2) = p_2;
+
+        match (x_1 < x_2, y_1 < y_2) {
+            (true, true) => Direction::NE,
+            (true, false) => Direction::SE,
+            (false, false) => Direction::SW,
+            (false, true) => Direction::NW,
+        }
+    }
+}
+
+fn exercise_2(antenna_pairs: &HashMap<char, Vec<(&AntennaCell, &AntennaCell)>>, bounds: (usize, usize)) -> Result<usize> {
+    let (n_rows, n_cols) = bounds;
+
+    let antinodes: HashSet<(usize, usize)> = antenna_pairs
+        .iter()
+        .flat_map(|(_, pairs)| -> Vec<(usize, usize)> {
+            pairs
+                .iter()
+                .flat_map(|(cell_1, cell_2)| -> Vec<(usize, usize)> {
+                    let (x_1, y_1) = cell_1.coords;
+                    let (x_2, y_2) = cell_2.coords;
+                    let mut antinodes = Vec::with_capacity(n_rows);
+                    // Add the two pairs as they are automatic antinodes
+                    antinodes.push((x_1, y_1));
+                    antinodes.push((x_2, y_2));
+
+                    // Determine the direction from the first to the second cell
+                    let direction = Direction::determine_direction((x_1, y_1), (x_2, y_2));
+                    // Push the elements in the direction of the pair.
+                    // First from 1 back and then from 2 forward.
+                    match direction {
+                        Direction::NE => {
+                            let delta_x = x_2 - x_1;
+                            let delta_y = y_2 - y_1;
+                            let (mut x_i, mut y_i) = (x_1, y_1);
+                            while x_i >= delta_x && y_i >= delta_y {
+                                x_i -= delta_x;
+                                y_i -= delta_y;
+                                antinodes.push((x_i, y_i));
+                            }
+                            let (mut x_i, mut y_i) = (x_2, y_2);
+                            while x_i + delta_x < n_cols && y_i + delta_y < n_rows {
+                                x_i += delta_x;
+                                y_i += delta_y;
+                                antinodes.push((x_i, y_i));
+                            }
+                        }
+                        Direction::SE => {
+                            let delta_x = x_2 - x_1;
+                            let delta_y = y_1 - y_2;
+                            let (mut x_i, mut y_i) = (x_1, y_1);
+                            while x_i >= delta_x && y_i + delta_y < n_rows {
+                                x_i -= delta_x;
+                                y_i += delta_y;
+                                antinodes.push((x_i, y_i));
+                            }
+                            let (mut x_i, mut y_i) = (x_2, y_2);
+                            while x_i + delta_x < n_cols && y_i >= delta_y {
+                                x_i += delta_x;
+                                y_i -= delta_y;
+                                antinodes.push((x_i, y_i));
+                            }
+                        }
+                        Direction::SW => {
+                            let delta_x = x_1 - x_2;
+                            let delta_y = y_1 - y_2;
+                            let (mut x_i, mut y_i) = (x_1, y_1);
+                            while x_i + delta_x < n_cols && y_i + delta_y < n_rows {
+                                x_i += delta_x;
+                                y_i += delta_y;
+                                antinodes.push((x_i, y_i));
+                            }
+                            let (mut x_i, mut y_i) = (x_2, y_2);
+                            while x_i >= delta_x && y_i >= delta_y {
+                                x_i -= delta_x;
+                                y_i -= delta_y;
+                                antinodes.push((x_i, y_i));
+                            }
+                        }
+                        Direction::NW => {
+                            let delta_x = x_1 - x_2;
+                            let delta_y = y_2 - y_1;
+                            let (mut x_i, mut y_i) = (x_1, y_1);
+                            while x_i + delta_x < n_cols && y_i >= delta_y {
+                                x_i += delta_x;
+                                y_i -= delta_y;
+                                antinodes.push((x_i, y_i));
+                            }
+                            let (mut x_i, mut y_i) = (x_2, y_2);
+                            while x_i >= delta_x && y_i + delta_y < n_cols {
+                                x_i -= delta_x;
+                                y_i += delta_y;
+                                antinodes.push((x_i, y_i));
+                            }
+                        }
+                    }
+                    antinodes
+                })
+                .collect()
+        })
+        .collect();
+
+    Ok(antinodes.len())
+}
+
+fn exercise_1(antenna_pairs: &HashMap<char, Vec<(&AntennaCell, &AntennaCell)>>, bounds: (usize, usize)) -> Result<usize> {
+    let (n_rows, n_cols) = bounds;
 
     let antinodes: HashSet<(usize, usize)> = antenna_pairs
         .iter()
@@ -117,7 +259,7 @@ fn exercise_1(antenna_grid: &AntennaGrid) -> Result<usize> {
                     );
                     let antinode_1 = match antinode_1 {
                         (Some(x), Some(y)) => {
-                            if x < antenna_grid.n_rows && y < antenna_grid.n_cols {
+                            if x < n_rows && y < n_cols {
                                 Some((x, y))
                             } else {
                                 None
@@ -127,7 +269,7 @@ fn exercise_1(antenna_grid: &AntennaGrid) -> Result<usize> {
                     };
                     let antinode_2 = match antinode_2 {
                         (Some(x), Some(y)) => {
-                            if x < antenna_grid.n_rows && y < antenna_grid.n_cols {
+                            if x < n_rows && y < n_cols {
                                 Some((x, y))
                             } else {
                                 None
@@ -148,9 +290,96 @@ fn exercise_1(antenna_grid: &AntennaGrid) -> Result<usize> {
 fn main() -> Result<()> {
     let input_str = fs::read_to_string("input.txt")?;
     let antenna_grid = AntennaGrid::from_str(&input_str)?;
+    let antennas = preprocessing(&antenna_grid)?;
+    let antenna_pairs = build_pairs(&antennas)?;
 
-    let result_1 = exercise_1(&antenna_grid)?;
+    let result_1 = exercise_1(&antenna_pairs, (antenna_grid.n_rows, antenna_grid.n_cols))?;
     println!("Result: {}", result_1);
 
+    let result_2 = exercise_2(&antenna_pairs, (antenna_grid.n_rows, antenna_grid.n_cols))?;
+    println!("Result: {}", result_2);
+
     Ok(())
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    use rstest::*;
+
+    #[fixture]
+    fn sample_input_string() -> &'static str {
+        "............
+........0...
+.....0......
+.......0....
+....0.......
+......A.....
+............
+............
+........A...
+.........A..
+............
+............"
+    }
+
+    #[fixture] 
+    fn sample_antennas() -> HashMap<char, Vec<&'static AntennaCell>> {
+        HashMap::from([
+            ('0', vec![
+                &AntennaCell { antenna: Some('0'), coords: (8, 1) },
+                &AntennaCell { antenna: Some('0'), coords: (5, 2) },
+                &AntennaCell { antenna: Some('0'), coords: (7, 3) },
+                &AntennaCell { antenna: Some('0'), coords: (4, 4) },
+            ]),
+            ('A', vec![
+                &AntennaCell { antenna: Some('A'), coords: (6, 5) },
+                &AntennaCell { antenna: Some('A'), coords: (8, 8) },
+                &AntennaCell { antenna: Some('A'), coords: (9, 9) },
+            ]),
+        ])
+    }
+
+    #[fixture]
+    fn sample_antenna_pairs() -> HashMap<char, Vec<(&'static AntennaCell, &'static AntennaCell)>> {
+        HashMap::from([
+            ('0', vec![
+                (&AntennaCell { antenna: Some('0'), coords: (8, 1) }, &AntennaCell { antenna: Some('0'), coords: (5, 2) }),
+                (&AntennaCell { antenna: Some('0'), coords: (8, 1) }, &AntennaCell { antenna: Some('0'), coords: (7, 3) }),
+                (&AntennaCell { antenna: Some('0'), coords: (8, 1) }, &AntennaCell { antenna: Some('0'), coords: (4, 4) }),
+                (&AntennaCell { antenna: Some('0'), coords: (5, 2) }, &AntennaCell { antenna: Some('0'), coords: (7, 3) }),
+                (&AntennaCell { antenna: Some('0'), coords: (5, 2) }, &AntennaCell { antenna: Some('0'), coords: (4, 4) }),
+                (&AntennaCell { antenna: Some('0'), coords: (7, 3) }, &AntennaCell { antenna: Some('0'), coords: (4, 4) }),
+            ]),
+            ('A', vec![
+                (&AntennaCell { antenna: Some('A'), coords: (6, 5) }, &AntennaCell { antenna: Some('A'), coords: (8, 8) }),
+                (&AntennaCell { antenna: Some('A'), coords: (6, 5) }, &AntennaCell { antenna: Some('A'), coords: (9, 9) }),
+                (&AntennaCell { antenna: Some('A'), coords: (8, 8) }, &AntennaCell { antenna: Some('A'), coords: (9, 9) }),
+            ]),
+        ])
+    }
+
+    #[rstest]
+    fn test_preprocessing(#[from(sample_input_string)] input_str: &'static str, #[from(sample_antennas)] expected_antennas: HashMap<char, Vec<&'static AntennaCell>>, #[from(sample_antenna_pairs)] expected_antenna_pairs: HashMap<char, Vec<(&'static AntennaCell, &'static AntennaCell)>>) {
+        let antenna_grid = AntennaGrid::from_str(input_str).unwrap();
+        let antennas = preprocessing(&antenna_grid).unwrap();
+        assert_eq!(antennas, expected_antennas);
+
+        let antenna_pairs = build_pairs(&antennas).unwrap();
+        assert_eq!(antenna_pairs, expected_antenna_pairs);
+    }
+
+    #[rstest]
+    fn test_exercise_1(#[from(sample_antenna_pairs)] antenna_pairs: HashMap<char, Vec<(&AntennaCell, &AntennaCell)>>) {
+        let result = exercise_1(&antenna_pairs, (12, 12)).unwrap();
+        assert_eq!(result, 14);
+    }
+
+    #[rstest]
+    fn test_exercise_2(#[from(sample_antenna_pairs)] antenna_pairs: HashMap<char, Vec<(&AntennaCell, &AntennaCell)>>) {
+        let result = exercise_2(&antenna_pairs, (12, 12)).unwrap();
+        assert_eq!(result, 34);
+    }
+
 }
